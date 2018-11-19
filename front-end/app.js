fc3aa7098e25e5b96b57314b954286b242009568
Clara Cheong <clarawhcheong@gmail.com>
2018-11-19 18:17:31 -0500

    update

--- front-end/app.js
+++ front-end/app.js
@@ -24,6 +24,22 @@ var lookscontroller = function($scope, $http, $routeParams) {
 lookscontroller.$inject = ['$scope', '$http','$routeParams'];
 angularApp.controller('lookscontroller', lookscontroller);
 
+var stylecontroller = function($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/casual'
+    }).then(function successCallback(response) {
+    console.log(response);
+    $scope.items = response.data;
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+}
+
+stylecontroller.$inject = ['$scope', '$http'];
+angularApp.controller('stylecontroller', stylecontroller);
+
 angularApp.config(function($routeProvider) {
     $routeProvider
     
@@ -35,6 +51,10 @@ angularApp.config(function($routeProvider) {
     .when("/looks/:style", {
         templateUrl: "looks.html",
         controller: "lookscontroller"
+    })
+    .when("/looks/casual", {
+        templateUrl: "casual.html",
+        controller: "stylecontroller"
     });
 });
 
