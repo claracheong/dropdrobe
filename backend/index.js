d9415fecbeb4e0585563642bebaf93e767748b57
Clara Cheong <clarawhcheong@gmail.com>
2018-11-19 17:22:42 -0500

    frontend changes

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,27 @@ app.get('/main', function(req, res) {
   res.status(200).send(data)
 });
 
+app.get('/looks/casual', function(req, res) {
+    const data = [{
+        look: 'elixaberh',
+        shoes: 'https://notredame.box.com/s/s2vcwd3xphq57flmmg2gsjj4pxozvq3l',
+        rings: 'https://notredame.box.com/s/vmx1j5mwq6fnefd13nb8g765c5jqcwky',
+        necklace: 'https://notredame.box.com/s/azdtffg08doldkaj1e6rlugxmqyqqsty',
+        outfit: 'https://notredame.box.com/s/ogo6po3798uq286653s52itzwxvf992b'},
+        {look: 'cheong',
+        shoes: 'https://notredame.box.com/s/s2vcwd3xphq57flmmg2gsjj4pxozvq3l',
+        rings: 'https://notredame.box.com/s/vmx1j5mwq6fnefd13nb8g765c5jqcwky',
+        necklace: 'https://notredame.box.com/s/azdtffg08doldkaj1e6rlugxmqyqqsty',
+        outfit: 'https://notredame.box.com/s/ogo6po3798uq286653s52itzwxvf992b'},
+        {look: 'lara',
+        shoes: 'https://notredame.box.com/s/s2vcwd3xphq57flmmg2gsjj4pxozvq3l',
+        rings: 'https://notredame.box.com/s/vmx1j5mwq6fnefd13nb8g765c5jqcwky',
+        necklace: 'https://notredame.box.com/s/azdtffg08doldkaj1e6rlugxmqyqqsty',
+        outfit: 'https://notredame.box.com/s/ogo6po3798uq286653s52itzwxvf992b'}]
+    res.status(200).send(data)
+});
+
+
 // in case of environmental variables for privacy
 const port = process.env.PORT || 3000;
 const httpServer = require('http').createServer(app);
